05939b3099b5f1b701bf97ae280e00bf25e528b0
maxime smolis <msm@webidea.fr>
2022-09-05 20:56:59 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Laravel</title>
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body class="antialiased">
+	{{-- https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false --}}
+	<h1>Hello world</h1>
+</body>
+
+</html>
