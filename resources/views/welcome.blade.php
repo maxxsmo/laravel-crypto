38938c0e7bd9f58f24c39abb13218f7a94e96167
maxime smolis <msm@webidea.fr>
2022-09-05 21:11:24 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,7 @@
 <body class="antialiased">
 	{{-- https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false --}}
 	<h1>Hello world</h1>
+
 </body>
 
 </html>
